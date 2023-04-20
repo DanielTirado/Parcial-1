4d0290ced6b6bad4eb9ed9871256173addaf5eed
DanielTirado <danieltirado088@gmail.com>
2023-04-20 01:05:26 -0500

    Lectura del archivo pensum funcional

--- codigo_parcial1/PARCIAL1/main.cpp
+++ codigo_parcial1/PARCIAL1/main.cpp
@@ -1,60 +1,127 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdlib.h>
 
 using namespace std;
 
-int main()
-{
-
-    // LECTURA DE LOS ARCHIVOS
+char **leer_materias(int (pensum)[46][5]){
     ifstream materias;
-    ifstream matricula;
-    ifstream calendar;
 
-    try {
+    char **codigo_nombre = new char*[46];
+
+    for (int i=0; i<46; i++){
+        codigo_nombre[i] = new char[26];
+    }
+
+    try{
         materias.open("materias.txt");
         if (!materias.is_open()) throw '1';
 
         // ALMACENAR EL CODIGO, LOS CREDITOS Y LAS HORAS DE CADA MATERIA DEL PENSUM EN UN ARREGLO INT
-        unsigned int pensum[46][5];
-        for (int i=0; i<46; i++){
-            for (int j=0; j<4; j++){
-                pensum[i][j]=0;
+
+        char linea[32];
+        int m=0;
+
+        while (materias.getline(linea, 32)){
+            int codigo=0;
+            int creditos=0;
+            int horas_docente=0;
+            int horas_independientes=0;
+            int horas_faltantes=0;
+
+            for (int i=0; i<7; i++){
+                codigo_nombre[m][i] = linea[i];
+                codigo = codigo*10+(linea[i])-'0';
+                pensum[m][0] = codigo;
             }
+
+            for (int i=7; i<24; i++){
+                codigo_nombre[m][i] = linea[i+1];
+            }
+
+            creditos = linea[26]-'0';
+            pensum[m][1] = creditos;
+
+            horas_docente = linea[28]-'0';
+            pensum[m][2] = horas_docente;
+
+            horas_independientes = 3*creditos;
+            horas_faltantes = horas_independientes;
+            pensum[m][3] = horas_independientes;
+            pensum[m][4] = horas_faltantes;
+            m++;
         }
-        // Mientras se almacenan los datos se hace el cálculo de cuántas horas independientes se necesitan
-        //...
-
-        //ALMACENAR EL CODIGO Y EL NOMBRE DE LA MATERIA EN UN ARREGLO CHAR
-
-        //...
-
         materias.close();
+    }
 
-        matricula.open("matricula.txt");
-        if (!matricula.is_open()) throw '2';
+    catch (char c){
+        cout << "Error #" << c <<": ";
+        if (c=='1') cout<<"Error al abrir el archivo de materias. \n";
+    }
+    catch (...){
+        cout << "Error no definido\n";
+    }
+    return codigo_nombre;
+}
 
+void leer_horario(int horario[6][18][7]){
+    ifstream calendar;
+
+    try {
         calendar.open("horario.txt");
-        if (!calendar.is_open()) throw '3';
-
-
-
-        // ALMACENAR TODOS LOS BLOQUES DIA-HORA-MATERIA EN UN ARREGLO INT
-        int horario[6][18][7];
-
-        for (int i=0; i<6; i++){
-            for (int j=0; j<18; j++){
-                for (int k=0; k<7; k++){
-                    horario[i][j][k] = 0;
-                }
-            }
+        if (!calendar.is_open()) throw '2';
+    }
+    catch (char c){
+        cout << "Error #"<<c<<": ";
+        if (c=='2'){
+            cout << "Error al abrir el archivo del horario.\n";
         }
-        //...
+    }
+    catch (...){
+        cout << "Error no definido\n";
+    }
+}
 
-        calendar.close();
+int main()
+{
+    ofstream archivo;
+    archivo.open("prueba.txt");
+    int pensum[46][5];
+    // LECTURA DE LAS MATERIAS
+    char **codigo_nombre = leer_materias(pensum);
 
-        bool programa = true;
+    cout << "Imprimiendo pensum...\n\n";
+    archivo << "Imprimiendo pensum...\n\n";
+    for (int i=0; i<46; i++){
+        for (int j=0; j<5; j++){
+            cout << pensum[i][j] << "\t";
+            archivo << pensum[i][j] << "\t";
+        }
+        cout <<endl;
+        archivo << endl;
+    }
+
+    cout <<"\n\nImprimiendo arreglo de codigo y nombre...\n\n";
+    archivo << "\n\nImprimiendo arreglo de codigo y nombre...\n\n";
+    for (int i=0; i<46; i++){
+        for (int j=0; j<26; j++){
+            cout<<codigo_nombre[i][j];
+            archivo<<codigo_nombre[i][j];
+        }
+        cout << endl;
+        archivo << endl;
+    }
+
+    archivo.close();
+    //LIBERAR MEMORIA
+
+    for (int i=0; i<46; i++){
+        delete[] codigo_nombre[i];
+    }
+    delete[] codigo_nombre;
+
+        /*bool programa = true;
         while (programa){
             cout << "\t\t\t ORGANIZADOR DE HORARIO \n\nBienvenido a tu organizador de horario, Que quieres hacer primero?\n\n";
 
@@ -105,23 +172,6 @@ int main()
 
             if (opcion == '4') programa = false;
 
-        }
-
-    }
-    catch (char c){
-        cout << "Error #" << c <<": ";
-        if (c=='1') cout<<"Error al abrir el archivo de materias. \n";
-
-        else if (c=='2') cout<<"Error al abrir el archivo de matricula. \n";
-
-        else if (c=='3') cout<<"Error al abrir el archivo del horario. \n";
-    }
-    catch (...){
-        cout << "Error no definido. \n";
-    }
-
-
-
-
+        }*/
     return 0;
 }
